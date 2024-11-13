7d54f5d0e956b5668984103d32ed1000a57241ca
Meir <0123meir@gmail.com>
2024-11-13 16:00:45 +0200

    new route

--- src/Controllers/routes.js
+++ src/Controllers/routes.js
@@ -2,14 +2,17 @@ const express = require('express');
 const db = require('../db')
 const router = express.Router();
 
-const newPost = router.post("/newpost",(req, res) => {
+const newPost = router.post("/newpost",async (req, res) => {
     try {
         const post = req.body;
     
         const insertedPost = await db.insertPost(post);
         res.json({
-          message: "Post created successfully!",
-          data: insertedPost, // Optionally include the inserted post data in the response
+          message: "Post created successfully!"
         });
-    }})
+    } catch (error) {
+        console.log(erorr);
+        
+    }
+})
 module.exports = {newPost}

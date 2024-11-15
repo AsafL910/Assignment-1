da2639eb40ce3d1d9614ebdb7f1390d770840a92
Meir <0123meir@gmail.com>
2024-11-15 14:27:54 +0200

    added findById API

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
 const db = require("./db/DbConnection");
-const { newPostRoute, getAllPostsRoute } = require("./Controllers/routes");
+const {
+  newPostRoute,
+  getAllPostsRoute,
+  getPostsByIdRoute,
+} = require("./Controllers/routes");
 
 app.use(express.json());
 app.post("/newPost", newPostRoute);
 app.get("/posts", getAllPostsRoute);
+app.get("/post/:id", getPostsByIdRoute);
 module.exports = app;

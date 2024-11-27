6951bf3a035dc22a8ef0fbc3da000e5de20e833e
Meir <0123meir@gmail.com>
2024-11-27 21:50:21 +0200

    added user schema

---
+++ src/db/commentSchema.js
@@ -0,0 +1,18 @@
+const commentSchema = new mongoose.Schema({
+    content: {
+      type: String,
+      required: true,
+    },
+    sender: {
+      type: String,
+      required: true,
+    },
+    postId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post", // Refers to the Post model
+      required: true,
+    },
+  });
+  
+  
+  module.exports = mongoose.model("Comment", commentSchema)
---
+++ src/db/userSchema.js
@@ -0,0 +1,13 @@
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  });
+  
+  userSchema.pre('save', async (next) => {
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+  });
+  
+  module.exports = mongoose.model('User', userSchema);
+  
